39d8c6fefc88b7f66f6ff0aea9350aeffc6c04d2
zhangjianping <zhangjianping@che001.com>
2017-08-10 10:05:19 +0800

    反转数组

---
+++ che1/echo2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * 反转数组
+ */
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	s := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(s)
+	reverse(s)
+	fmt.Println(s)
+}
